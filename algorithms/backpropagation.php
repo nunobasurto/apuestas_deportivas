8ae4656bb24b0fa959cd7c916ac07ab181208764
nunobasurto <nbh0007@alu.ubu.es>
2016-11-10 12:58:40 +0100

    Update backpropagation.php
    Prueba red sencilla

--- algorithms/backpropagation.php
+++ algorithms/backpropagation.php
@@ -192,4 +192,10 @@ class Neuron
         return $inputs[$index];
     }
 }
+$nn = new NeuralNetwork(2,2,2, [0.15,0.2,0.25,0.3], 0.35, [0.4,0.45,0.5,0.55], 0.6);
+for ($i=0; $i <10000; $i++) { 
+    $nn->train([0.05, 0.1], [0.01, 0.99]);
+    echo $i . ' ' . round($nn->calculate_total_error([[[0.05,0.1], [0.01,0.99]]]),9);
+}
+
 ?>
