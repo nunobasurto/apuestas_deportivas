1fd19377e8c7bbbd77852480708d74948f457672
nunobasurto <nbh0007@alu.ubu.es>
2016-11-15 18:05:30 +0100

    Funcionamiento completo del algoritmo

--- algorithms/scrapClasif.php
+++ algorithms/scrapClasif.php
@@ -9,17 +9,46 @@
  	$trs = $html->getElementsByTagName("tr");
  	$flag=False;
 	foreach($trs as $tr){
-		$posicion = $tr->getElementsByTagName("th")->item(0)->nodeValue;
+		$posicion = (int)$tr->getElementsByTagName("th")->item(0)->nodeValue;
 		
-		if($posicion=='1'){
+		if($posicion==1){
 			$flag=True;
 		}
 		$equipo = $tr->getElementsByTagName("td")->item(0)->nodeValue;
 		if($flag==True AND !empty($equipo)){
-			echo 'Pos ' . $posicion . ' ' . utf8_decode($equipo);
+			/*echo 'Pos ' . $posicion . ' ' . utf8_decode($equipo);
+			echo "<br>" . PHP_EOL;*/
+			$puntos = (int)$tr->getElementsByTagName("td")->item(1)->nodeValue;
+			$jugados = (int)$tr->getElementsByTagName("td")->item(2)->nodeValue;
+			$victorias =(int) $tr->getElementsByTagName("td")->item(3)->nodeValue;
+			$empates = (int)$tr->getElementsByTagName("td")->item(4)->nodeValue;
+			$derrotas = (int)$tr->getElementsByTagName("td")->item(5)->nodeValue;
+			$favor = (int)$tr->getElementsByTagName("td")->item(6)->nodeValue;
+			$contra = (int)$tr->getElementsByTagName("td")->item(7)->nodeValue;
+			
+			$equipo = substr($equipo,0,strlen($equipo)-1);
+			echo $posicion . ' ' . $equipo . ' ' . $puntos . ' ' . $jugados . ' ' . $victorias . ' ' . $empates . ' ' . $derrotas . ' ' . $favor . ' ' . $contra;
 			echo "<br>" . PHP_EOL;
+
+			$update = db_update('equipos')
+			->fields(array(
+				'posicion' => $posicion,
+				'puntos' => $puntos,
+				'jugados'=> $jugados,
+				'victorias'=> $victorias,
+				'empates'=> $empates,
+				'derrotas'=> $derrotas,
+				'favor'=> $favor,
+				'contra'=> $contra,
+				))
+			->condition('nombreCompleto', $equipo ,'=')
+			->execute();
+			
+			/*$args = array(':posicion' => $posicion, ':equipo' => $equipo);
+			$art = db_query('UPDATE {equipos} SET posicion=:posicion  WHERE nombreCompleto = :equipo', 
+				array(':posicion'=>$posicion, ':equipo'=>$equipo))->fetchField();*/
 		}
-		if($posicion == '20'){
+		if($posicion == 20){
 			$flag=False;
 			break;
 		}
