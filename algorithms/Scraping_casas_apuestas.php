e109a87f13e339e75ff75a73f826ff984f03a08f
nunobasurto <nbh0007@alu.ubu.es>
2017-01-19 19:50:16 +0100

    Código adaptado a la nueva versión de la página web

--- algorithms/Scraping_casas_apuestas.php
+++ algorithms/Scraping_casas_apuestas.php
@@ -7,68 +7,60 @@ drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 $tiempo = getdate();
 $currentTyme= $tiempo["year"] . '-' . $tiempo["mon"] . '-' . $tiempo["mday"] . ' 00:00:00';
 $jornada  = db_query('SELECT f.jornada FROM {fecha_jornada} f WHERE f.fecha_antes > :ff ORDER BY f.fecha_antes asc', array(':ff' => $currentTyme))->fetchField(); 
-$url = 'http://www.resultados-futbol.com/primera/grupo1/jornada' . "$jornada";
-echo $jornada;
-$source = file_get_contents($url);
-libxml_use_internal_errors(true);
-libxml_clear_errors();
-$html = new DOMDocument();
-$html->loadHTML($source);
-$xpath=new DOMXpath($html);
-$trs = $html->getElementsByTagName("tr");
-$flag=true;
+$jor_aux = $jornada*100;
 
 $id_prim = $jornada*100 + 1;
 $control = db_query('SELECT a.BET365 FROM {apuestas} a WHERE a.id_partido = :id', array(':id' => $id_prim))->fetchField();
 
-//Control para que no lo vuelva a ejecutar si los datos ya han sido almacenados.
-if ($control == null){
-foreach($trs as $tr){
- 	$nameStat = $tr->getElementsByTagName("div"); 		
- 	$eLocal = $tr->getElementsByTagName("td")->item(2)->nodeValue;
- 	$eVisitante = $tr->getElementsByTagName("td")->item(4)->nodeValue;
- 	$id_partido;
- 	$id_1;
- 	$id_2;
- 	$id_3;
- 	$id_4;
+if($control == null){
+for ($i = 1; $i <= 10; $i++) {
+    $id = $jor_aux + $i;
+    //Extreaemos los equipos local y visitante para utilizarlos en la url.
+    $equipolocal  = db_query('SELECT e.nombre FROM {fecha_jornada} f, {equipos} e WHERE f.id_partido = :id AND f.equipo_local = e.id_equipo', array(':id' => $id))->fetchField();
+    $equipovisitante  = db_query('SELECT e.nombre FROM {fecha_jornada} f, {equipos} e WHERE f.id_partido = :id AND f.equipo_visitante = e.id_equipo', array(':id'=>$id))->fetchField();
+    //Generamos dos arrays vacíos donde iremos metiendo los datos.
+    //Establecemos la url como un String.
+	$url = 'http://www.resultados-futbol.com/partido/'. $equipolocal .'/'.$equipovisitante;
+	echo $equipolocal . ' - ' . $equipovisitante;
+	echo '</br>' . PHP_EOL;
+    $source = file_get_contents($url);
+ 	libxml_use_internal_errors(true);
+ 	libxml_clear_errors();
+ 	$html = new DOMDocument();
+ 	$html->loadHTML($source);
+ 	$xpath=new DOMXpath($html);
+ 	$divs = $html->getElementsByTagName("div");
 
- 	if(!empty($eLocal)AND!empty(trim($eVisitante))AND strlen($eVisitante)>3 AND $eVisitante !='Posición'){
- 		$eLocal = trim($eLocal);
- 		$eVisitante = trim($eVisitante);
-		$id_local = db_query('SELECT e.id_equipo FROM {equipos} e WHERE e.nombreCompleto = :eLocal', array(':eLocal' => $eLocal))->fetchField();
-		$id_visitante = db_query('SELECT e.id_equipo FROM {equipos} e WHERE e.nombreCompleto = :eVisitante', array(':eVisitante' => $eVisitante))->fetchField();
-		$id_partido = db_query('SELECT f.id_partido FROM {fecha_jornada} f WHERE f.equipo_local = :id_local AND f.equipo_visitante = :id_visitante', array(':id_local' => $id_local, ':id_visitante' =>$id_visitante))->fetchField();
+ 	$id_1 = $id*10+1;
+ 	$id_2 = $id*10+2;
+ 	$id_3 = $id*10+3;
+ 	$id_4 = $id*10+4;
 
- 		$id_par =  "$id_partido";
- 		$id_1 = (int)($id_par . '1');
- 		$id_2 = (int)($id_par . '2');
- 		$id_3 = (int)($id_par . '3');
- 		$id_4 = (int)($id_par . '4');
-
-
- 		$ins=db_insert('apuestas')
-			->fields(array(
-			'id_partido' => $id_partido,
-			'BET365'=>null,
-			'Marca'=>null,
-			'BWIN'=>null,
-			'888Bet'=>null,
-			))
-			->execute();
-
+ 	$ins=db_insert('apuestas')
+		->fields(array(
+		'id_partido' => $id,
+		'BET365'=>null,
+		'Marca'=>null,
+		'BWIN'=>null,
+		'888Bet'=>null,
+		))
+		->execute();
+	
  		$flag1 = True;
-- 		$flag2 = True;
-- 		$flag3 = True;
-- 		$flag4 = True;
- 		}
-		foreach($nameStat as $stat){
+ 		$flag2 = True;
+ 		$flag3 = True;
+ 		$flag4 = True;
+ 		
+		foreach($divs as $stat){
 			$var=$stat->getElementsByTagName("a")->item(0)->nodeValue;
 			
 			if ($var =='Bet365'AND $flag1 ==true){
 				$Local=substr($stat->getElementsByTagName("a")->item(1)->nodeValue,2,4);
 				$Empate=substr($stat->getElementsByTagName("a")->item(2)->nodeValue,2,4);
 				$Visitante=substr($stat->getElementsByTagName("a")->item(3)->nodeValue,2,4);
+				echo 'Bet365 ';
+				echo $Local . ' - ' . $Empate . ' - ' . $Visitante;
+				echo '</br>' . PHP_EOL;
 				$Local = str_replace(',', '.' , $Local);
 				$Empate = str_replace(',', '.' , $Empate);
 				$Visitante = str_replace(',', '.' , $Visitante);
@@ -85,7 +77,7 @@ foreach($trs as $tr){
 				->fields(array(
 				'BET365' => $id_1,
 				))
-				->condition('id_partido', $id_partido, '=')
+				->condition('id_partido', $id, '=')
 				->execute();
 				
 			}
@@ -93,6 +85,9 @@ foreach($trs as $tr){
 				$Local=substr($stat->getElementsByTagName("a")->item(1)->nodeValue,2,4);
 				$Empate=substr($stat->getElementsByTagName("a")->item(2)->nodeValue,2,4);
 				$Visitante=substr($stat->getElementsByTagName("a")->item(3)->nodeValue,2,4);
+				echo 'Marca ';
+				echo $Local . ' - ' . $Empate . ' - ' . $Visitante;
+				echo '</br>' . PHP_EOL;
 				$Local = str_replace(',', '.' , $Local);
 				$Empate = str_replace(',', '.' , $Empate);
 				$Visitante = str_replace(',', '.' , $Visitante);
@@ -109,14 +104,16 @@ foreach($trs as $tr){
 				->fields(array(
 				'Marca' => $id_2,
 				))
-				->condition('id_partido', $id_partido, '=')
+				->condition('id_partido', $id, '=')
 				->execute();
-				$flag2=false;
 			}
 			elseif ($var =='bwin'AND $flag3 ==true){
 				$Local=substr($stat->getElementsByTagName("a")->item(1)->nodeValue,2,4);
 				$Empate=substr($stat->getElementsByTagName("a")->item(2)->nodeValue,2,4);
 				$Visitante=substr($stat->getElementsByTagName("a")->item(3)->nodeValue,2,4);
+				echo 'BWIN ';
+				echo $Local . ' - ' . $Empate . ' - ' . $Visitante;
+				echo '</br>' . PHP_EOL;
 				$Local = str_replace(',', '.' , $Local);
 				$Empate = str_replace(',', '.' , $Empate);
 				$Visitante = str_replace(',', '.' , $Visitante);
@@ -133,7 +130,7 @@ foreach($trs as $tr){
 				->fields(array(
 				'BWIN' => $id_3,
 				))
-				->condition('id_partido', $id_partido, '=')
+				->condition('id_partido', $id, '=')
 				->execute();
 				
 			}
@@ -141,6 +138,9 @@ foreach($trs as $tr){
 				$Local=substr($stat->getElementsByTagName("a")->item(1)->nodeValue,2,4);
 				$Empate=substr($stat->getElementsByTagName("a")->item(2)->nodeValue,2,4);
 				$Visitante=substr($stat->getElementsByTagName("a")->item(3)->nodeValue,2,4);
+				echo '888 sports ';
+				echo $Local . ' - ' . $Empate . ' - ' . $Visitante;
+				echo '</br>' . PHP_EOL;
 				$Local = str_replace(',', '.' , $Local);
 				$Empate = str_replace(',', '.' , $Empate);
 				$Visitante = str_replace(',', '.' , $Visitante);
@@ -157,12 +157,12 @@ foreach($trs as $tr){
 				->fields(array(
 				'888Bet' => $id_4,
 				))
-				->condition('id_partido', $id_partido, '=')
+				->condition('id_partido', $id, '=')
 				->execute();
 				
 			}
 		}
 	}
 }
-	header("Location:/informe-pronostico");
+header("Location:/informe-pronostico");
 ?>
